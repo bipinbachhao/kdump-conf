e9d63647b67a99889db0f56f91461a4ea473e833
bipin bachhao <bipinbachhao@gmail.com>
2017-09-24 14:45:25 -0400

    Centos-6 part working. Needs more work for centos-7

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default['kdump-conf']['nfs_server'] = 'bipin.local.com'
-default['kdump-conf']['dump_location'] = 'kernel-dumps'
+default['kdump-conf']['dump_corepath'] = '/dumps'
+default['kdump-conf']['dump_location'] = "#{node['kdump-conf']['dump_corepath']}/kernel-dumps"
 default['kdump-conf']['dump_on_remote'] = true
 default['kdump-conf']['dump_local_path'] = '/var/core-dump'
--- recipes/kdump-conf.rb
+++ recipes/kdump-conf.rb
@@ -12,6 +12,7 @@
 require 'mixlib/shellout'
 
 nfs_server = node['kdump-conf']['nfs_server']
+dump_corepath = node['kdump-conf']['dump_corepath']
 dump_location =  node['kdump-conf']['dump_location']
 dump_on_remote = node['kdump-conf']['dump_on_remote']
 dump_local_path = node['kdump-conf']['dump_local_path']
@@ -31,3 +32,67 @@ crash_kernel_size = if ram < 2048
                     else
                       'auto'
                     end
+
+if (node['platform'] == 'redhat') || (node['platform'] == 'centos')
+  if (node['platform_version'] >= '6') && (node['platform_version'] < '7')
+
+    # Centos 6 Part
+    ruby_block 'Adding_kdump_configuration_in_grub_file' do
+      block do
+        file = Chef::Util::FileEdit.new('/boot/grub/grub.conf')
+        file.search_file_replace(/crashkernel=[[:graph:]]+/, "crashkernel=#{crash_kernel_size}")
+        file.write_file
+      end
+    end
+
+    blklist = 'blacklist dm_multipath hpwdt ata_piix libiscsi_tcp qla4xxx libiscsi scsi_transport_iscsi'
+
+    if dump_on_remote
+      crashtype = 'net'
+      corepath = dump_corepath
+      msglevel = ' --message-level 15'
+      location = "#{nfs_server}:#{dump_location}"
+    else
+      crashtype = crash_filesystem
+      corepath = dump_local_path
+      location = root_disk
+      msglevel = ''
+    end
+
+    template '/etc/kdump.conf' do
+      source 'kdump.conf.erb'
+      mode '0644'
+      variables(
+        crashtype: crashtype,
+        location: location,
+        corepath: corepath,
+        msglevel: msglevel,
+        blklist: blklist
+      )
+    end
+
+  else
+    # Centos 7 Part
+
+    ruby_block 'Adding_kdump_configuration_in_grub_file' do
+      block do
+        file = Chef::Util::FileEdit.new('/etc/default/grub')
+        file.search_file_replace(/crashkernel=[[:graph:]]+/, "crashkernel=#{crash_kernel_size}")
+        file.write_file
+      end
+    end
+
+    execute 'grub2-mkconfig' do
+      command 'grub2-mkconfig -o /boot/grub2/grub.cfg'
+      action :run
+    end
+
+    ruby_block 'kdump_driver_blacklist_append' do
+      block do
+        file = Chef::Util::FileEdit.new(/etc/sysconfig/kdump)
+        file.search_file_replace(/(^KDUMP_COMMANDLINE_APPEND.*)/, )
+      end
+    end
+
+  end
+end
